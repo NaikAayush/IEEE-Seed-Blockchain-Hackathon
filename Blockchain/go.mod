24860755f8352dd8a29d3efacf120f2c583b0679
Samyak S Sarnayak <samyak201@gmail.com>
2020-10-31 15:13:27 +0530

    [BLOCKCHAIN] new working code lol

--- Blockchain/go.mod
+++ Blockchain/go.mod
@@ -3,9 +3,17 @@ module SeedChain
 go 1.14
 
 require (
-  github.com/golang/protobuf v1.3.2
-  github.com/hyperledger/fabric-chaincode-go v0.0.0-20200424173110-d7076418f212
-  github.com/hyperledger/fabric-contract-api-go v1.1.0
-  github.com/hyperledger/fabric-protos-go v0.0.0-20200424173316-dd554ba3746e
-  github.com/stretchr/testify v1.5.1
+	github.com/fsouza/go-dockerclient v1.6.6 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2 // indirect
+	github.com/hyperledger/fabric v1.4.9
+	github.com/hyperledger/fabric-amcl v0.0.0-20200424173818-327c9e2cf77a // indirect
+	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
+	github.com/spf13/viper v1.7.1 // indirect
+	github.com/sykesm/zap-logfmt v0.0.4 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
+	google.golang.org/grpc v1.33.1 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
--- Blockchain/seedBlock.go
+++ Blockchain/seedBlock.go
@@ -1,19 +1,140 @@
 package main
 
 import (
-  "log"
-
-  "github.com/hyperledger/fabric-contract-api-go/contractapi"
-  "SeedChain/chaincode"
+	"fmt"
+	// "encoding/json"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-func main() {
-  assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
-  if err != nil {
-    log.Panicf("Error creating asset-transfer-basic chaincode: %v", err)
-  }
 
-  if err := assetChaincode.Start(); err != nil {
-    log.Panicf("Error starting asset-transfer-basic chaincode: %v", err)
-  }
+// Seed structure
+type SeedInfo struct {
+	ID string `json:"ID"`
+	Name string `json:"name"`
 }
+
+// SeedChaincode implementation of Chaincode
+type SeedChaincode struct {
+}
+
+// Init of the chaincode
+// This function is called only one when the chaincode is instantiated.
+// So the goal is to prepare the ledger to handle future requests.
+func (t *SeedChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("########### SeedChaincode Init ###########")
+
+	// Get the function and arguments from the request
+	function, _ := stub.GetFunctionAndParameters()
+
+	// Check if the request is the init function
+	if function != "init" {
+		return shim.Error("Unknown function call")
+	}
+
+	// Put in the ledger the key/value hello/world
+	err := stub.PutState("hello", []byte("world"))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	// Return a successful message
+	return shim.Success(nil)
+}
+
+// Invoke
+// All future requests named invoke will arrive here.
+func (t *SeedChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	fmt.Println("########### SeedChaincode Invoke ###########")
+
+	// Get the function and arguments from the request
+	function, args := stub.GetFunctionAndParameters()
+
+	// Check whether it is an invoke request
+	if function != "invoke" {
+		return shim.Error("Unknown function call")
+	}
+
+	// Check whether the number of arguments is sufficient
+	if len(args) < 1 {
+		return shim.Error("The number of arguments is insufficient.")
+	}
+
+	// In order to manage multiple type of request, we will check the first argument.
+	// Here we have one possible argument: query (every query request will read in the ledger without modification)
+	if args[0] == "query" {
+		return t.query(stub, args)
+	}
+
+	// The update argument will manage all update in the ledger
+	if args[0] == "invoke" {
+		return t.invoke(stub, args)
+	}
+
+	// If the arguments given don’t match any function, we return an error
+	return shim.Error("Unknown action, check the first argument")
+}
+
+// query
+// Every readonly functions in the ledger will be here
+func (t *SeedChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	fmt.Println("########### SeedChaincode query ###########")
+
+	// Check whether the number of arguments is sufficient
+	if len(args) < 2 {
+		return shim.Error("The number of arguments is insufficient.")
+	}
+
+	// Like the Invoke function, we manage multiple type of query requests with the second argument.
+	// We also have only one possible argument: hello
+
+	// Get the state of the value matching the key hello in the ledger
+	state, err := stub.GetState(args[1])
+	if err != nil {
+		return shim.Error("Failed to get state of hello")
+	}
+
+	// Return this value in response
+	return shim.Success(state)
+}
+
+// invoke
+// Every functions that read and write in the ledger will be here
+func (t *SeedChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	fmt.Println("########### SeedChaincode invoke ###########")
+
+	if len(args) < 2 {
+		return shim.Error("The number of arguments is insufficient.")
+	}
+
+	// Check if the ledger key is "hello" and process if it is the case. Otherwise it returns an error.
+	if len(args) == 3 {
+
+		// Write the new value in the ledger
+		err := stub.PutState(args[1], []byte(args[2]))
+		if err != nil {
+			return shim.Error("Failed to update state of hello")
+		}
+
+		// Notify listeners that an event "eventInvoke" have been executed (check line 19 in the file invoke.go)
+		err = stub.SetEvent("eventInvoke", []byte{})
+		if err != nil {
+			return shim.Error(err.Error())
+		}
+
+		// Return this value in response
+		return shim.Success(nil)
+	}
+
+	// If the arguments given don’t match any function, we return an error
+	return shim.Error("Unknown invoke action, check the second argument.")
+}
+
+func main() {
+	// Start the chaincode and make it ready for futures requests
+	err := shim.Start(new(SeedChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Seed Service chaincode: %s", err)
+	}
+}
+
