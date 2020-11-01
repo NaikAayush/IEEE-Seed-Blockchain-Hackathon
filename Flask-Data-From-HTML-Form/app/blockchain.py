a3be77ace8deb43d5f9be523eb240f8c94a5c383
NaikAayush <ayush@pesu.pes.edu>
2020-11-01 07:38:46 +0530

    changed endpoints

--- Flask-Data-From-HTML-Form/app/blockchain.py
+++ Flask-Data-From-HTML-Form/app/blockchain.py
@@ -5,7 +5,23 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()
 
-url = os.getenv("URL")
+# url = os.getenv("URL")
+# url = os.getenv("URL")
+
+
+SPA_URL=os.getenv("SPA_URL")
+SPA_CC=os.getenv("SPA_CC")
+SPA=os.getenv("SPA")
+SCA_URL=os.getenv("SCA_URL")
+SCA=os.getenv("SCA")
+RET_URL=os.getenv("RET_URL")
+RET=os.getenv("RET")
+FAR_URL=os.getenv("FAR_URL")
+FAR=os.getenv("FAR")
+STL_URL=os.getenv("STL_URL")
+STL=os.getenv("STL")
+chaincodeVer = "v1"
+
 
 headers = {
   'Content-Type': 'application/json',
@@ -34,28 +50,28 @@ def invoke(data):
 
     payload = {
         "args": ["invoke", data["ID"], json.dumps(data)],
-        "chaincode": chaincode,
-        "channel": channel,
+        "chaincode": SPA_CC,
+        "channel": SPA,
         "chaincodeVer": chaincodeVer,
         "method": "invoke"
     }
 
-    response = requests.request("POST", url, headers=headers, json=payload)
+    response = requests.request("POST", SPA_URL, headers=headers, json=payload)
 
     return json.loads(response.text.encode('utf8'))['result']['payload']
 
-def updateTest(data):
+def updateTest(ID,data):
     data = {k:v for k,v in data.items() if k in stl_keys}
 
     payload = {
         "args": ["updateTest", ID, json.dumps(data)],
-        "chaincode": chaincode,
-        "channel": channel,
+        "chaincode": STL,
+        "channel": STL,
         "chaincodeVer": chaincodeVer,
         "method": "invoke"
     }
 
-    response = requests.request("POST", url, headers=headers, json=payload)
+    response = requests.request("POST", STL_URL, headers=headers, json=payload)
 
     return json.loads(response.text.encode('utf8'))['result']['payload']
 
@@ -63,13 +79,13 @@ def updateCertification(ID, data):
     data = {k:v for k,v in data.items() if k in sca_keys}
     payload = {
         "args": ["updateCertification", ID, json.dumps(data)],
-        "chaincode": chaincode,
-        "channel": channel,
+        "chaincode": SCA,
+        "channel": SCA,
         "chaincodeVer": chaincodeVer,
         "method": "invoke"
     }
 
-    response = requests.request("POST", url, headers=headers, json=payload)
+    response = requests.request("POST", SCA_URL, headers=headers, json=payload)
 
     return json.loads(response.text.encode('utf8'))['result']['payload']
 
@@ -77,26 +93,26 @@ def updateDist(ID, data):
     data = {k:v for k,v in data.items() if k in dist_keys}
     payload = {
         "args": ["updateDist", ID, json.dumps(data)],
-        "chaincode": chaincode,
-        "channel": channel,
+        "chaincode": RET,
+        "channel": RET,
         "chaincodeVer": chaincodeVer,
         "method": "invoke"
     }
 
-    response = requests.request("POST", url, headers=headers, json=payload)
+    response = requests.request("POST", RET_URL, headers=headers, json=payload)
 
     return json.loads(response.text.encode('utf8'))['result']['payload']
 
 def getHistory(ID):
     payload = {
         "args": ["getHistory", ID],
-        "chaincode": chaincode,
-        "channel": channel,
+        "chaincode": FAR,
+        "channel": FAR,
         "chaincodeVer": chaincodeVer,
         "method": "invoke"
     }
 
-    response = requests.request("POST", url, headers=headers, json=payload)
+    response = requests.request("POST", FAR_URL, headers=headers, json=payload)
 
     return json.loads(response.text.encode('utf8'))['result']['payload']
 
