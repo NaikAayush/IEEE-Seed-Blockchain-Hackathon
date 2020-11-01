76059d882064095574fca176d8a79da38051ae55
NaikAayush <ayush@pesu.pes.edu>
2020-11-01 10:27:42 +0530

    b

--- Flask-Data-From-HTML-Form/app/app.py
+++ Flask-Data-From-HTML-Form/app/app.py
@@ -556,6 +556,9 @@ def spp_update():
 
     return render_template("spp_update_seed.html",login_type=session["login_type"])
 
+@app.route("/review")
+def review():
+    return render_template("review.html")
 
 """# handle login failed
 @app.errorhandler(401)
