bac121074eb9aee8b91fbfd5d59637d40976abf8
NaikAayush <ayush@pesu.pes.edu>
2020-11-10 18:07:08 +0530

    added DDL statements

---
+++ Blockchain/mysql.sql
@@ -0,0 +1,10 @@
+USE seeds
+
+CREATE TABLE seed (
+    uuid VARCHAR(50),
+    lotNumber VARCHAR(50),
+    sampleSecretCode VARCHAR(50),
+    tagSeries VARCHAR(50),
+    tagStart INT,
+    tagEnd INT
+)
